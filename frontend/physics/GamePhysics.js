fdd030d1c1c6dcf0448326ca11cdabb92fb8250d
AkaTFL <coldway3108@gmail.com>
2026-03-23 14:29:24 +0100

    fix : déplacement de la logique bump dans la classe bumper

--- frontend/physics/GamePhysics.js
+++ frontend/physics/GamePhysics.js
@@ -1,5 +1,4 @@
 import * as RAPIER from "@dimforge/rapier3d-compat"
-import Config from './Config.js';
 
 export class GamePhysics {
     constructor(config) {
@@ -39,40 +38,9 @@ export class GamePhysics {
 
             for (let bumper of this.bumpers) {
                 if (bumper.collider.handle === handle1 || bumper.collider.handle === handle2) {
-                    this.applyBumperForce(bumper, handle1, handle2)
+                    bumper.applyBumperForce(handle1, handle2)
                 }
             }
         })
-    }
-
-    applyBumperForce(bumper, handle1, handle2) {
-        const otherHandle = bumper.collider.handle === handle1 ? handle2 : handle1
-        const otherCollider = this.world.colliders.get(otherHandle)
-        if (!otherCollider) return
-
-        const otherBody = otherCollider.parent()
-        if (!otherBody) return
-
-        const bumperPos = bumper.rigidBody.translation()
-        const ballPos = otherBody.translation()
-
-        const dirX = ballPos.x - bumperPos.x
-        const dirY = ballPos.y - bumperPos.y
-        const dirZ = ballPos.z - bumperPos.z
-
-        // Normaliser
-        const length = Math.sqrt(dirX * dirX + dirY * dirY + dirZ * dirZ)
-        if (length === 0) return
-
-        const X = dirX / length
-        const Y = dirY / length
-        const Z = dirZ / length
-
-        // Appliquer force
-        const power = Config.bumper.power * Config.forceMultiplier
-        otherBody.applyImpulse(
-            { x: X * power, y: Y * power, z: Z * power },
-            true
-        )
     }
 }
