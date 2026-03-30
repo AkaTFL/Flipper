a060eb3acb77e7b2f01f38d4a94b6d1cc63070c3
AkaTFL <coldway3108@gmail.com>
2026-03-30 18:16:05 +0200

    refacto : Changement de l'instanciation de flipper.js pour plus de clarté 'passage à un tableau mesh'

--- frontend/core/Flipper.js
+++ frontend/core/Flipper.js
@@ -21,87 +21,40 @@ async function initFlipper() {
     const container = document.getElementById('three');
     container.appendChild(sceneManager.renderer.domElement);
 
-    const wallR = new Wall(physics.world, 950, 100, { x: 255, y: 0, z: 0 }, { x: 0, y: (Math.PI / 2), z: 0 });
-    const wallL = new Wall(physics.world, 950, 100, { x: -255, y: 0, z: 0 }, { x: 0, y: (-Math.PI / 2), z: 0 });
-    const wallT = new Wall(physics.world, 540, 100, { x: 0, y: 0, z: -471 }, { x: 0, y: 0, z: 0 });
-    const wallB = new Wall(physics.world, 540, 100, { x: 0, y: 0, z: 471 }, { x: 0, y: 0, z: 0 });
+    const mesh = [];
 
-    const launchingRamp = new LaunchingRamp(physics.world, 30, 10, 850, { x: -230, y: 10, z: -50 }, { x: (Math.PI / 2), y: 0, z: 0 });
-    controls.setLaunchingRampRef(launchingRamp);
+    mesh.push(new Wall(physics.world, 950, 100, { x: 255, y: 0, z: 0 }, { x: 0, y: (Math.PI / 2), z: 0 }));
+    mesh.push(new Wall(physics.world, 950, 100, { x: -255, y: 0, z: 0 }, { x: 0, y: (-Math.PI / 2), z: 0 }));
+    mesh.push(new Wall(physics.world, 540, 100, { x: 0, y: 0, z: -471 }, { x: 0, y: 0, z: 0 }));
+    mesh.push(new Wall(physics.world, 540, 100, { x: 0, y: 0, z: 471 }, { x: 0, y: 0, z: 0 }));
 
-    const bumper1 = new Bumper(physics.world, 50, { x: 0, y: 0, z: 100 }, {x: 0, y: 0, z: 0}, 'bumper-1');
-    const bumper2 = new Bumper(physics.world, 50, { x: 100, y: 0, z: 0 }, {x: 0, y: 0, z: 0}, 'bumper-2');
-    const bumper3 = new Bumper(physics.world, 50, { x: -100, y: 0, z: 0 }, {x: 0, y: 0, z: 0}, 'bumper-3');
+    mesh.push(new LaunchingRamp(physics.world, 30, 10, 850, { x: -230, y: 10, z: -50 }, { x: (Math.PI / 2), y: 0, z: 0 }));
+    controls.setLaunchingRampRef(mesh[mesh.length - 1]);
 
-    const palles1 = new Palles(physics.world, 70, 10, 10, { x: 100, y: 10, z: -400 }, { x: 0, y: 0, z: 0 }, 'left');
-    const palles2 = new Palles(physics.world, 70, 10, 10, { x: -100, y: 10, z: -400 }, { x: 0, y: 0, z: 0 }, 'right');
+    mesh.push(new Bumper(physics.world, 50, { x: 0, y: 0, z: 100 }, {x: 0, y: 0, z: 0}, 'bumper-1'));
+    mesh.push(new Bumper(physics.world, 50, { x: 100, y: 0, z: 0 }, {x: 0, y: 0, z: 0}, 'bumper-2'));
+    mesh.push(new Bumper(physics.world, 50, { x: -100, y: 0, z: 0 }, {x: 0, y: 0, z: 0}, 'bumper-3'));
 
-    // Enregistrer les bumpers dans le système physique
-    physics.registerBumper(bumper1);
-    physics.registerBumper(bumper2);
-    physics.registerBumper(bumper3);
-    physics.registerLaunchingRamp(launchingRamp);
+    mesh.push(new Palles(physics.world, 70, 10, 10, { x: 100, y: 10, z: -400 }, { x: 0, y: 0, z: 0 }, 'left'));
+    mesh.push(new Palles(physics.world, 70, 10, 10, { x: -100, y: 10, z: -400 }, { x: 0, y: 0, z: 0 }, 'right'));
 
-    const ball = new Ball(physics.world, { x: -230, y: 25, z: -400 });
-    controls.setBallRef(ball);
+    physics.registerObjects(mesh);
 
-    sceneManager.scene.add(ball.mesh);
-    sceneManager.scene.add(wallR.mesh);
-    sceneManager.scene.add(wallL.mesh);
-    sceneManager.scene.add(wallT.mesh);
-    sceneManager.scene.add(wallB.mesh);
-    sceneManager.scene.add(...launchingRamp.meshes);
-    sceneManager.scene.add(bumper1.mesh);
-    sceneManager.scene.add(bumper2.mesh);
-    sceneManager.scene.add(bumper3.mesh);
-    sceneManager.scene.add(palles1.mesh);
-    sceneManager.scene.add(palles2.mesh);
- 
+    mesh.push(new Ball(physics.world, { x: -230, y: 25, z: -400 }));
+    controls.setBallRef(mesh[mesh.length - 1]);
+
+    sceneManager.scene.add(...mesh);
+
     sceneManager.startRender(physics, () => {
-      if (controls.getLaunchChargeCount() < 0) controls.setLaunchChargeCount(0);
+      controls.setLaunchChargeCount(0);
 
-        palles1.syncPalle();
-        palles2.syncPalle();
-        ball.syncBall();
+      mesh[mesh.length - 4].syncPalle();
+      mesh[mesh.length - 3].syncPalle();
+      mesh[mesh.length - 1].syncBall();
 
-        palles1.setActive(controls.input.left);
-        palles2.setActive(controls.input.right);
-        
+      mesh[mesh.length - 4].setActive(controls.input.left);
+      mesh[mesh.length - 3].setActive(controls.input.right);
     });
 }
 
 initFlipper();
-
-
-// import { Scene } from './Scene.js';
-// import { Ball } from '../objects/Ball.js';
-// import Config from '../physics/Config.js';
-// import { GamePhysics } from '../physics/GamePhysics.js';
-// import { Wall } from '../objects/Wall.js';
-
-
-// async function initFlipper() {
-//     const physics = new GamePhysics(Config);
-//     await physics.init();
-
-//     const sceneManager = new Scene(physics.world, 500, 500, { x: 0, y: 500, z: 0 });
-
-//     const container = document.getElementById('three');
-//     container.appendChild(sceneManager.renderer.domElement);
-
-//     const mesh = [];
-
-//     mesh.push(new Wall(500, 500, { x: 250, y: 500, z: 0 }));
-//     mesh.push(new Wall(500, 500, { x: -250, y: 500, z: 0 }));
-//     mesh.push(new Wall(500, 500, { x: 0, y: 750, z: 0 }));
-//     mesh.push(new Wall(500, 500, { x: 0, y: 250, z: 0 }));
-
-//     mesh.push(new Ball(physics.world, { x: 0, y: 500, z: 0 }))
-
-//     sceneManager.scene.add(...mesh);
-
-//     sceneManager.startRender(physics, () => mesh[mesh.length - 1].syncBall());
-// }
-
-// // Start the game
-// initFlipper();
