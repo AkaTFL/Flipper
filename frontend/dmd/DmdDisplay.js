8a192563d965ba5e0151196eb772d9a30d9b4208
Raphael Personnel <pharosi.raphael@gmail.com>
2026-06-18 16:28:28 +0200

    fix: agrandir l'affichage DMD

--- frontend/dmd/DmdDisplay.js
+++ frontend/dmd/DmdDisplay.js
@@ -206,9 +206,9 @@ export class DmdDisplay {
 
         this.resizeFrame = globalThis.requestAnimationFrame(() => {
             const lines = [
-                { element: this.titleEl, widthRatio: 0.9 },
-                { element: this.mainEl, widthRatio: 0.82 },
-                { element: this.subEl, widthRatio: 0.9 }
+                { element: this.titleEl, widthRatio: 0.94 },
+                { element: this.mainEl, widthRatio: 0.9 },
+                { element: this.subEl, widthRatio: 0.94 }
             ];
 
             for (const line of lines) {
