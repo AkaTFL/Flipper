b7c8828457e98c92c2115efbbab26c3bc7f3bfa6
AkaTFL <coldway3108@gmail.com>
2026-06-23 10:54:17 +0200

    dd

--- frontend/flipper/physics/Config.js
+++ frontend/flipper/physics/Config.js
@@ -109,13 +109,13 @@ export default {
                         model: `../assets/mesh/bumpers/bumper_lvl_${NiveauActuel}.glb`
                     },
 
-                    {
-                        width: 70,
-                        position: { x: -120.5, y: 20, z: -441 },
-                        rotation: { x: 0, y: -(Math.PI / 3), z: 0 },
-                        objectId: 'bumper-triangle-left',
-                        model: `../assets/mesh/bumpers_triangle/bumper_triangle_left_lvl_${NiveauActuel}.glb`
-                    },
+                    // {
+                    //     width: 70,
+                    //     position: { x: -120.5, y: 20, z: -441 },
+                    //     rotation: { x: 0, y: -(Math.PI / 3), z: 0 },
+                    //     objectId: 'bumper-triangle-left',
+                    //     model: `../assets/mesh/bumpers_triangle/bumper_triangle_left_lvl_${NiveauActuel}.glb`
+                    // },
                     // {
                     //     width: 70,
                     //     position: { x: 110.5, y: 20, z: -441 },
