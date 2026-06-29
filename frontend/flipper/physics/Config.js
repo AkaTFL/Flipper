b58f152471d089dd6047adc4e3152f5e0d30ec88
AkaTFL <coldway3108@gmail.com>
2026-06-29 19:54:29 +0200

    fix config

--- frontend/flipper/physics/Config.js
+++ frontend/flipper/physics/Config.js
@@ -88,7 +88,7 @@ export default {
                         position: { x: -30, y: 10, z: 270 },
                         rotation: { x: 0, y: 0, z: 0 },
                         objectType: 'bumper',
-                        objectId: 'bumper-4',
+                        objectId: 'bumper-5',
                         model: `../assets/mesh/bumpers/bumper_lvl_${NiveauActuel}.glb`
                     },
 
@@ -99,7 +99,7 @@ export default {
                         position: { x: 240, y: 60, z: 317 },
                         rotation: { x: 0, y: 0, z: 0 },
                         objectType: 'bumper',
-                        objectId: 'bumper-4',
+                        objectId: 'bumper-6',
                         model: `../assets/mesh/bumpers/bumper_lvl_${NiveauActuel}.glb`
                     },
                     {
@@ -107,7 +107,7 @@ export default {
                         position: { x: 120, y: 60, z: 317 },
                         rotation: { x: 0, y: 0, z: 0 },
                         objectType: 'bumper',
-                        objectId: 'bumper-5',
+                        objectId: 'bumper-7',
                         model: `../assets/mesh/bumpers/bumper_lvl_${NiveauActuel}.glb`
                     },
                     {
@@ -115,7 +115,7 @@ export default {
                         position: { x: 180, y: 60, z: 217 },
                         rotation: { x: 0, y: 0, z: 0 },
                         objectType: 'bumper',
-                        objectId: 'bumper-6',
+                        objectId: 'bumper-8',
                         model: `../assets/mesh/bumpers/bumper_lvl_${NiveauActuel}.glb`
                     },
                     {
@@ -123,7 +123,7 @@ export default {
                         position: { x: 195, y: 60, z: 117 },
                         rotation: { x: 0, y: 0, z: 0 },
                         objectType: 'bumper',
-                        objectId: 'bumper-7',
+                        objectId: 'bumper-9',
                         model: `../assets/mesh/bumpers/bumper_lvl_${NiveauActuel}.glb`
                     },
                     {
@@ -131,7 +131,7 @@ export default {
                         position: { x: 250, y: 60, z: 157 },
                         rotation: { x: 0, y: 0, z: 0 },
                         objectType: 'bumper',
-                        objectId: 'bumper-8',
+                        objectId: 'bumper-10',
                         model: `../assets/mesh/bumpers/bumper_lvl_${NiveauActuel}.glb`
                     },
 
@@ -166,7 +166,7 @@ export default {
                         position: { x: 245, y: 10, z: -670 },
                         rotation: { x: 0, y: (Math.PI / 2), z: (Math.PI / 2) },
                         objectType: 'repulse',
-                        objectId: 'repulse-zone',
+                        objectId: 'repulse-zone-1',
                         model: '../assets/mesh/Repulse.glb'
                     },
                     {
@@ -308,7 +308,7 @@ export default {
                 },
                 {
                     model: `../assets/mesh/murs_cible_left/murs_cible_left_lvl_${NiveauActuel}.glb`,
-                    objectId: 'murs-cible-left',
+                    objectId: 'murs-cible-left-2',
                     objectType: 'wall',
                     length: 50,
                     width: 50,
@@ -330,7 +330,7 @@ export default {
                 },
                 {
                     model: `../assets/mesh/murs_cible_left/murs_cible_left_lvl_${NiveauActuel}.glb`,
-                    objectId: 'murs-cible-droit',
+                    objectId: 'murs-cible-droit-2',
                     objectType: 'wall',
                     length: 50,
                     width: 50,
